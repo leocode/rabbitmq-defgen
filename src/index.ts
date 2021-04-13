50b5797d1eb72b4134036a96b64d76d2d05f8ef7
Mateusz Koteja <soanvig@gmail.com>
2021-04-13 13:00:23 +0200

    Add bin and build script

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
+#!/usr/bin/env node
 import { Option, program } from 'commander';
 import { processFile } from './processFile';
 const packageJson = require('../package.json');
 
 program
   .version(packageJson.version)
-  .addOption(new Option('-o, --output <outputType>', 'output needs to be defined').choices(['json', 'terraform']))
-  .addOption(new Option('-i, --input <inputPath>', 'input path needs to be defined'))
+  .addOption(new Option('-o, --output <outputType>', 'output needs to be defined').choices(['json', 'terraform']).makeOptionMandatory())
+  .requiredOption('-i, --input <inputPath>', 'input path needs to be defined')
   .parse();
 
 const options = program.opts();
